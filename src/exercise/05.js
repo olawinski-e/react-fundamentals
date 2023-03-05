2e0e6d6c7515f6a82139a4918cd9242eeabfe22f
Estelle Olawinski <estelle.olawinski@me.com>
2023-03-05 20:48:46 +0100

    step5: styling

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,16 +14,93 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// -------------- V1
+// const smallBox = (
+//   <div
+//     className="box box--small"
+//     style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+//   >
+//     small lightblue box
+//   </div>
+// )
+// const mediumBox = (
+//   <div
+//     className="box box--medium"
+//     style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+//   >
+//     medium pink box
+//   </div>
+// )
+// const largeBox = (
+//   <div
+//     className="box box--large"
+//     style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+//   >
+//     large orange box
+//   </div>
+// )
 
-function App() {
+// const App = () => {
+//   return (
+//     <div>
+//       {smallBox}
+//       {mediumBox}
+//       {largeBox}
+//     </div>
+//   )
+// }
+
+// -------------- V2
+// const Box = ({className = '', style, ...props}) => {
+//   return (
+//     <div
+//       className={`box ${className}`}
+//       style={{fontStyle: 'italic', ...style}}
+//       {...props}
+//     />
+//   )
+// }
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+//         small lightblue box
+//       </Box>
+//       <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+//         medium pink box
+//       </Box>
+//       <Box className="box--large" style={{backgroundColor: 'orange'}}>
+//         large orange box
+//       </Box>
+//     </div>
+//   )
+// }
+
+// -------------- V3
+const Box = ({size, style, ...props}) => {
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${sizeClassName}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...props}
+    />
+  )
+}
+
+const App = () => {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
+      <Box size="medium" style={{backgroundColor: 'pink'}}>
+        medium pink box
+      </Box>
+      <Box size="large" style={{backgroundColor: 'orange'}}>
+        large orange box
+      </Box>
     </div>
   )
 }
