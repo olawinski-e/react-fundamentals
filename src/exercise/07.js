0ae7226f37f26c1741849fe433af8e2e95d39297
Estelle Olawinski <estelle.olawinski@me.com>
2023-03-05 21:52:17 +0100

    step7: keys on li

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -10,7 +10,7 @@ const allItems = [
   {id: 'pear', value: '🍐 pear'},
 ]
 
-function App() {
+const App = () => {
   const [items, setItems] = React.useState(allItems)
 
   function addItem() {
@@ -30,7 +30,7 @@ function App() {
       <ul>
         {items.map(item => (
           // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
